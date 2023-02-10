a94b9288307d481fd2f9b4b0cae76e196b6ac230
Ani Mkheidze <ani@my-clay.com>
2023-02-10 10:15:36 +0100

    Add project files.

---
+++ trying4/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace trying_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "v1", "v2" };
+        }
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id) { return "value"; }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) { }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
+    }
+}
